99a409b5b4442059645510e7c873d563cfecce2c
Bondifrench <d_dumaine@yahoo.com>
2014-05-29 22:30:21 +1000

    improving the display of the RSS link

--- client/js/app.js
+++ client/js/app.js
@@ -1,48 +1,52 @@
 'use strict';
 
-var News = angular.module('myApp', ['ngSanitize']);
-console.log('Loading controllers');
+var myApp = angular.module('myApp', ['ngSanitize']);
+console.log('Loading App Controllers');
 
 
-News.controller('GoogCtrl', ['$scope','YQLService', function($scope, Feed){
-	$scope.loadButtonText = 'Choose News Feed';
+myApp.controller('NewsCtrl', ['$scope','YQLService', function($scope, Feed){
+	console.log('Loading News Controllers');
+	$scope.loadButtonText = 'Choose News Feed ';
 	$scope.RSSList = [
-		{Title: "CNN",
+		{Title: "CNN ",
 		url: 'http://rss.cnn.com/rss/cnn_topstories.rss'},
-		{Title: "SEC new USGAAP XBRL reports",
+		{Title: "SEC new USGAAP XBRL reports ",
 		url: 'http://www.sec.gov/Archives/edgar/usgaap.rss.xml'},
-		{Title: "Reuters US Markets",
+		{Title: "Reuters US Markets ",
 		url: 'http://feeds.reuters.com/news/usmarkets'},
-		{Title: "CNBC Top News",
+		{Title: "CNBC Top News ",
 		url: 'http://www.cnbc.com/id/100003114/device/rss/rss.html'},
-		{Title: "CNBC Earnings",
+		{Title: "CNBC Earnings ",
 		url: 'http://www.cnbc.com/id/15839135/device/rss/rss.html'},
-		{Title: "CNBC Europe News",
+		{Title: "CNBC Europe News ",
 		url: 'http://www.cnbc.com/id/19794221/device/rss/rss.html'},
-		{Title: "CNBC Investing",
+		{Title: "CNBC Investing ",
 		url: 'http://www.cnbc.com/id/15839069/device/rss/rss.html'},
-		{Title: "Reuters World News",
+		{Title: "Reuters World News ",
 		url: 'http://feeds.reuters.com/Reuters/worldNews'},
-		{Title: "Reuters Bonds",
+		{Title: "Reuters Bonds ",
 		url: 'http://feeds.reuters.com/reuters/bondsNews'},
-		{Title: "Reuters Basic Materials",
+		{Title: "Reuters Basic Materials ",
 		url: 'http://feeds.reuters.com/reuters/basicmaterialsNews'},
-		{Title: "Reuters Industrials",
+		{Title: "Reuters Industrials ",
 		url: 'http://feeds.reuters.com/reuters/industrialsNews'},
-		{Title: "Reuters Technology",
+		{Title: "Reuters Technology ",
 		url: 'http://feeds.reuters.com/reuters/technologysectorNews'},
-		{Title: "Reuters US TMT",
+		{Title: "Reuters US TMT ",
 		url: 'http://feeds.reuters.com/reuters/UStechnologyTelcomNews'},
-		{Title:'Bloomberg Views',
+		{Title:'Bloomberg Views ',
 		url:'http://www.bloombergview.com/rss'},
-		{Title: 'Seeking Alpha Latest',
+		{Title: 'Seeking Alpha Latest ',
 		url: 'http://seekingalpha.com/feed.xml'},
-		{Title: 'Seeking Alpha Transcripts',
+		{Title: 'Seeking Alpha Transcripts ',
 		url: 'http://seekingalpha.com/sector/transcripts.xml'},
-		{Title: 'Seeking Alpha Long Ideas',
+		{Title: 'Seeking Alpha Long Ideas ',
 		url: 'http://seekingalpha.com/tag/long-ideas.xml'}
 	];
+	//Adding pagination
 	$scope.itemsPerPage=10;
+
+	//Loading the news items
 	$scope.loadFeed = function (url, e) {
 		//e is just short for event, any variable can be put actually
 		//is used by jQuery
@@ -61,7 +65,7 @@ News.controller('GoogCtrl', ['$scope','YQLService', function($scope, Feed){
 			});
 	}	
 }]);
-News.factory('GoogService', ['$http', function($http){
+myApp.factory('GoogService', ['$http', function($http){
 	console.log('Use of Google factory');
 	return {
 		parseFeed: function (url){
@@ -69,11 +73,27 @@ News.factory('GoogService', ['$http', function($http){
 		}
 	}
 }]);
-News.factory('YQLService', ['$http', function($http){
+myApp.factory('YQLService', ['$http', function($http){
 	console.log('Use of Yahoo factory');
 	return {
 		parseFeed: function (url){
 			return $http.jsonp('//query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22'+encodeURIComponent(url)+'%22&format=json&callback=JSON_CALLBACK');
+		}
+	}
+}]);
+
+myApp.controller('StockCtrl', ['$scope', 'YQLStockService', function($scope, Stock){
+	var ticker='CSCO';
+	Stock.stockFeed(ticker).then(function (res) {
+		console.log(res.data.query.results.quote);
+		$scope.stocks= res.data.query.results.quote;
+	});
+}]);
+myApp.factory('YQLStockService', ['$http', function($http){
+	console.log('Use of Yhoo stock quotes factory');
+	return {
+		stockFeed: function (ticker){
+			return $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22'+ticker+'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK');
 		}
 	}
 }]);
